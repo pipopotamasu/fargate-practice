35562da8732cd7508d461838067d8efff97acbdd
pipopotamasu <ec0156hx@gmail.com>
2018-11-05 22:48:32 +0900

    502問題の対応

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
-# デフォルトは localhost
-# 既にAPP_SERVERという環境変数が与えられていればそれを使い、なければlocalhostを使う
-[ -z "$APP_SERVER" ] && APP_SERVER=localhost
+# 既にAPP_SERVERという環境変数が与えられていればそれを使い、なければ127.0.0.1を使う
+[ -z "$APP_SERVER" ] && APP_SERVER=127.0.0.1
 
 # nginx.conf の該当設定箇所を文字列置換
 sed "s|%APP_SERVER%|$APP_SERVER|g" -i /etc/nginx/conf.d/default.conf
